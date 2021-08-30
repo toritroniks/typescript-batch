2cbc7eb6da79bee08d4a8fd2a6ff1929831451bb
Ravi Souza <ravi@techfirm.co.jp>
2021-08-30 17:25:18 +0900

    improvements

--- src/libs/helpers/batchHelper.ts
+++ src/libs/helpers/batchHelper.ts
@@ -1,19 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import 'source-map-support/register';
 import Ajv, { Schema } from 'ajv';
 import exceptions from '@libs/enums/exceptions';
 
 export async function baseHandler<T>(handler: BatchHandler<T>, batchOptions?: BatchOptions) {
   console.log('START:', process.argv[1]);
+  let exitCode = 0;
   try {
     const event = JSON.parse(process.argv[2] ?? '{}');
     console.log('EVENT:', JSON.stringify(event, null, 2));
     if (batchOptions?.reqSchema) validateRequest(batchOptions.reqSchema, event);
-    handler(event);
+    await handler(event);
   } catch (err) {
     console.error(err);
+    exitCode = 1;
   }
   console.log('END');
-  return;
+  process.exit(exitCode);
 }
 
 function validateRequest(schema: Schema, req: unknown) {
@@ -29,7 +32,7 @@ function validateRequest(schema: Schema, req: unknown) {
   }
 }
 
-export type BatchHandler<T> = (event: T) => Promise<void>;
+export type BatchHandler<T> = (event: T) => Promise<void> | void;
 
 export interface BatchOptions {
   reqSchema?: any;
--- webpack.config.ts
+++ webpack.config.ts
@@ -4,51 +4,56 @@ import * as nodeExternals from 'webpack-node-externals';
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 import { getEntries } from '@libs/helpers/webpackHelper';
 
-const config: webpack.Configuration = {
-  context: __dirname,
-  mode: 'production', // In case of local build this is overriden by the cli --mode option
-  entry: getEntries(),
-  output: {
-    libraryTarget: 'commonjs',
-    path: path.join(__dirname, 'dist'),
-    filename: '[name]',
-    clean: true,
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.mjs', '.json', '.ts'],
-    symlinks: false,
-    cacheWithContext: false,
+// eslint-disable-next-line max-lines-per-function
+const config: (env: BuildEnv) => webpack.Configuration = env => {
+  return {
+    context: __dirname,
+    mode: 'production', // In case of local build this is overriden by the cli --mode option
+    entry: getEntries(),
+    output: {
+      libraryTarget: 'commonjs',
+      path: path.join(__dirname, 'dist'),
+      filename: '[name]',
+      clean: true,
+    },
+    devtool: 'source-map',
+    resolve: {
+      extensions: ['.mjs', '.json', '.ts'],
+      plugins: [
+        new TsconfigPathsPlugin({
+          configFile: './tsconfig.paths.json',
+        }),
+      ],
+    },
+    target: 'node',
+    externals: [nodeExternals() as any],
+    module: {
+      rules: [
+        // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+        {
+          test: /\.(tsx?)$/,
+          loader: 'ts-loader',
+          exclude: [[path.resolve(__dirname, 'node_modules'), path.resolve(__dirname, 'dist')]],
+          options: {
+            transpileOnly: true,
+            experimentalWatchApi: true,
+          },
+        },
+      ],
+    },
     plugins: [
-      new TsconfigPathsPlugin({
-        configFile: './tsconfig.paths.json',
+      new webpack.DefinePlugin({
+        'process.env.ENV': JSON.stringify(env.name ?? 'local'),
+      }),
+      new webpack.ProvidePlugin({
+        $config: [path.resolve(path.join(__dirname, 'src/libs/config')), 'default'],
       }),
     ],
-  },
-  optimization: {
-    concatenateModules: false,
-  },
-  target: 'node',
-  externals: [nodeExternals() as any],
-  module: {
-    rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      {
-        test: /\.(tsx?)$/,
-        loader: 'ts-loader',
-        exclude: [[path.resolve(__dirname, 'node_modules'), path.resolve(__dirname, 'dist')]],
-        options: {
-          transpileOnly: true,
-          experimentalWatchApi: true,
-        },
-      },
-    ],
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $config: [path.resolve(path.join(__dirname, 'src/libs/config')), 'default'],
-    }),
-  ],
+  };
 };
+
+interface BuildEnv {
+  name: string | null;
+}
 
 export default config;
